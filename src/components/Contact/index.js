c8619943fdba4bb671f3a6f611a8a9cbebf6c945
MBrassey <luc1d_life@protonmail.com>
2020-12-18 22:26:07 -0700

    Contact Component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,24 @@
+function ContactForm() {
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
