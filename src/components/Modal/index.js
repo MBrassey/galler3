83dc852c3e6bf77ab5b065881e1d13d7d294d3ae
MBrassey <luc1d_life@protonmail.com>
2020-12-18 23:12:32 -0700

    Added Modal Component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Modal({currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
+  
+    return (
+      <div className="modalBackdrop">
+        <div className="modalContainer">
+          <h3 className="modalTitle">{name}</h3>
+          <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+          <p>{description}</p>
+        <button type="button">Close this modal</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
